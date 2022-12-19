592727d9ab3f6984e276a831d960c0bb4040901d
Alex Eastman <alexeast@buffalo.edu>
2022-12-18 20:11:08 -0500

    updated CMakeLists.txt . added displayit header file for user inclusion

---
+++ displayit.hpp
@@ -0,0 +1,10 @@
+#ifndef DISPLAYIT_DISPLAYIT_HPP_
+#define DISPLAYIT_DISPLAYIT_HPP_
+// Author: Alex Eastman
+// Date: 12/18/22
+// File: displayit.hpp
+//
+// The primary file that users should include to expose the displayit
+// library API.
+
+#endif  // DISPLAYIT_DISPLAYIT_HPP_
