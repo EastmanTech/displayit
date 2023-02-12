4a06d9ff14b5deee270d40f43459880376c07b28
Alex Eastman <alexeast@buffalo.edu>
2023-02-12 18:51:38 -0500

    created displayit.cpp file

---
+++ src/displayit.cpp
@@ -0,0 +1,10 @@
+/**
+ * @author Alex Eastman
+ * @date 02/12/2023
+ * @copyright Unlicense License
+ * @file displayit.cpp
+ * The primary file defining functionality for the displayit library.
+ * This file brings all of the functionality together from across files. It
+ * is the entry point for program execution.
+ * 
+*/
